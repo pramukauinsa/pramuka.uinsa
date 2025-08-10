cc796f091e5692256b430f6a8007ad3ddcf52994
pramukauinsa <racana.uinsby@gmail.com>
2025-08-10 22:14:19 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// Menu mobile toggle
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
